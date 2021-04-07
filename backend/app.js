4071d595f440875860482bfe4b4c9fc80d2a57bd
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-04-08 00:07:17 +0100

    Consulta e cadastrar

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,90 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('./models/home')
+const Home = mongoose.model('Home')
+
+require('./models/orcamento')
+const Orcamento = mongoose.model('Orcamento')
+
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/celke', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true}).then(() => {
+        console.log('Conexão com MONGODB realizada com sucesso!!!');
+    }).catch((err) => {
+        console.log('Conexão com MONGODB NÃO realizada com sucesso!!!');
+    })
+
+app.get('/home', async (req, res) => {
+    await Home.findOne({}).then((home) => {
+        return res.json({
+            error: false,
+            home
+        })
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true,
+            message: 'Nenhum registro encontrado'
+        })
+    })
+})
+
+app.post('/home', async (req, res) => {
+    const dados = {
+        'topTitulo': 'Temos a solução que a sua empresa precisa',
+        'topSubtitulo': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text,',
+        'topTextoBtn': 'Orçamento',
+        'topLinkBtn': '/orcamento',
+        'serTitulo': 'Serviços',
+        'serSubyitulo': 'Lorem Ipsum is simply dummy text of the printing industry.',
+        'serUmIcone': 'laptop-code',
+        'serUmTitulo': 'Serviço Um',
+        'serUmDesc': 'Lorem Ipsum is simply dummy text of the printing industry.',
+        'serDoisIcone': 'mobile-alt',
+        'serDoisTitulo': 'Serviço Dois',
+        'serDoisDesc': 'Lorem Ipsum is simply dummy text of the printing industry.',
+        'serTresIcone': 'netwok-wired',
+        'serTresTitulo': 'Serviço Tres',
+        'serTresDesc': 'Lorem Ipsum is simply dummy text of the printing industry.',
+    }
+
+    const homeExiste = await Home.findOne({})
+    if(homeExiste) {
+        return res.status(400).json({
+            error: true,
+            message: 'Error: A página Home já possui registros!'
+        })
+    }
+
+    await Home.create(dados, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: 'Error: Conteúdo da página Home NÃO cadastrado com secesso!'
+        })
+    })
+    return res.json({
+        error: false,
+        message: 'Conteúdo da página Home CADSATRADO com secesso!'
+    })
+})
+
+app.post('/orcamento', async (req, res) => {
+    
+
+    await Orcamento.create(req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: 'Error: Solicitação de orçamento não enviada com secesso!'
+        })
+    })
+    return res.json({
+        error: false,
+        message: 'Solicitação de orçamento enviada com secesso!'
+    })
 })
 
 app.listen(8080, function(){
---
+++ backend/models/orcamento.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const orcamento = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    whatsup: {
+        type: String
+    },
+
+    projeto: {
+        type: String
+    },
+}, {
+    timestamps: true
+})
+
+mongoose.model('Orcamento', orcamento)
