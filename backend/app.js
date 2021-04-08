b80d5ebdf8721aa4960deb0e67ce8322f79699e7
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-04-08 10:31:05 +0100

    API implementada

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 
 require('./models/home')
 const Home = mongoose.model('Home')
@@ -10,6 +12,14 @@ const Orcamento = mongoose.model('Orcamento')
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization')
+    app.use(cors())
+    next()
+})
 
 mongoose.connect('mongodb://localhost:27017/celke', {
     useNewUrlParser: true,
